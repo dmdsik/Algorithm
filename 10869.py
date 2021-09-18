7af6b9016024214745ebcc94a74b0dc633bd0f7a
dmdsik <dmdsik@naver.com>
2021-09-18 16:26:55 +0900

    Create 10869.py
    Division part must be output an integer form, The int() coverd one more

---
+++ 10869.py
@@ -0,0 +1,15 @@
+A, B = input().split()
+
+A = int(A)
+B = int(B)
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B)) 
+'''
+Division part must be output an integer form,
+int() coverd one more
+'''
+
+print(A%B)
