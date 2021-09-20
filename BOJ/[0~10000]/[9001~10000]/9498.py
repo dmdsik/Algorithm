93cfd60482410c8d83abb6f00210af834416d579
dmdsik <dmdsik@naver.com>
2021-09-21 00:00:37 +0900

    Create 9498.py

---
+++ BOJ/[0~10000]/[9001~10000]/9498.py
@@ -0,0 +1,24 @@
+Score = int(input())
+
+'''
+if 100>= Score >=90:
+    print('A')
+elif 90> Score >=80:
+    print('B')
+elif 80> Score >=70:
+    print('C')
+elif 70> Score >=60:
+    print('D')
+else:
+    print('F')
+'''
+if Score >=90:
+    print('A')
+elif Score >=80:
+    print('B')
+elif Score >=70:
+    print('C')
+elif Score >=60:
+    print('D')
+else:
+    print('F')
